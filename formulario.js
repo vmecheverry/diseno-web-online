2a9d759f168ad5486e8fe6cdb50729b6bf26384f
vmecheverry <echeverryleon@hotmail.com>
2015-07-28 22:35:30 -0500

    Clase 4
    Siguiendo paso a paso la clase 4

---
+++ formulario.js
@@ -0,0 +1,31 @@
+var $form=$("#formulario"),
+$titulo=$("#titulo"),
+$url=$("#url"),
+$button=$('mostrar-form'),
+$list=$('#contenido'),
+$post=$('.item').first();
+
+
+function mostrarFormulario(){
+	$form.slideToggle();
+	return false;
+}
+function agregarPost (){
+	alert("hola");
+	var url=$url.val(),
+	titulo=$titulo.val(),
+	$clone =$post.clone();
+	$clone.find('.titulo_item a').text(titulo)
+	.attr('href',url);
+
+	$clone.hide();
+
+	$list.prepend($clone);
+	$clone.fadeIn();
+	return false;
+}
+
+// Eventos
+
+$button.click(mostrarFormulario);
+$form.on('submit',agregarPost);
